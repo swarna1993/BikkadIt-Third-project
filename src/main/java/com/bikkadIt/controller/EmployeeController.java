4dcde2ffccddd896b747390dbd9827277b992df0
swarna1993 <swarna.krisanya@gmail.com>
2022-08-08 06:01:51 +0530

    Employee class added

---
+++ src/main/java/com/bikkadIt/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.bikkadIt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+	
+
+}
---
+++ src/main/java/com/bikkadIt/model/Employee.java
@@ -0,0 +1,42 @@
+package com.bikkadIt.model;
+
+public class Employee {
+	
+	private int empId;
+	
+	private String empName;
+	
+	private String empAddress;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpAddress() {
+		return empAddress;
+	}
+
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empAddress=" + empAddress + "]";
+	}
+	
+	
+
+}
